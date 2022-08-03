82ef4c70c9f53f4d93dc69f205a149b88f071d46
Артём Одийко <odiykoartem@gmail.com>
2022-08-03 11:08:47 +0300

    Output pokemon data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,55 @@
+import React from "react";
 import type { NextPage } from "next";
+import Image from "next/image";
+
 import { trpc } from "@/utils/trpc";
+import { getOptionsForVote } from "@/utils/getRandomPokemon";
+import { inferQueryResponse } from "./api/trpc/[trpc]";
+
+const btn =
+	"inline-flex items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
 
 const Home: NextPage = () => {
+	const [ids, updateIds] = React.useState(() => getOptionsForVote());
+
+	const [first, second] = ids;
+
+	const firstPokemon = trpc.useQuery(["get-pokemon-by-id", { id: first }]);
+	const secondPokemon = trpc.useQuery(["get-pokemon-by-id", { id: second }]);
+
+	const voteForRoundest = (selected: number) => {
+		updateIds(getOptionsForVote());
+	};
+
 	return (
 		<main className="h-screen w-screen flex flex-col justify-center items-center">
 			<h1 className="text-2xl text-center pt-8 m-3">Which Pokémon is Rounder?</h1>
-			<div className="border rounded p-8 flex justify-between items-center max-w-2xl">
-				<div className="w-16 h-16 bg-red-200"></div>
-				<div className="p-8">Vs</div>
-				<div className="w-16 h-16 bg-red-200"></div>
+			<div className="p-8 flex justify-between items-center max-w-2xl">
+				{!firstPokemon.isLoading && firstPokemon.data && !secondPokemon.isLoading && secondPokemon.data && (
+					<>
+						<PokemonListing pokemon={firstPokemon.data} vote={() => voteForRoundest(first)} />
+						<div className="p-8">Vs</div>
+						<PokemonListing pokemon={secondPokemon.data} vote={() => voteForRoundest(second)} />
+					</>
+				)}
 			</div>
 		</main>
+	);
+};
+
+type PokemonFromServer = inferQueryResponse<"get-pokemon-by-id">;
+
+const PokemonListing: React.FC<{ pokemon: PokemonFromServer; vote: () => void }> = ({ pokemon, vote }) => {
+	return (
+		<div className="flex flex-col items-center">
+			<p className="text-xl text-center capitalize">{pokemon.name}</p>
+			<div className="w-64 h-64 relative">
+				<Image layout="fill" objectFit="cover" src={pokemon.sprites.front_default} alt={pokemon.name} />
+			</div>
+			<button className={btn} type="button" onClick={vote}>
+				Rounder
+			</button>
+		</div>
 	);
 };
 
